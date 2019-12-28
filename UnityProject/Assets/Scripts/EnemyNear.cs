5937843dc58ec06f7e6ab8ff20ad2affb3a9f60a
Elbert-Eddie <Elbert-Eddie@users.noreply.github.com>
2019-12-28 20:16:09 +0800

    殭屍移動

---
+++ UnityProject/Assets/Scripts/EnemyNear.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class EnemyNear : Enemy
+{
+    public override void Move()
+    {
+        agent.SetDestination(player.position);      // 代理器.設定目的地(玩家.座標)
+        ani.SetBool("跑步開關",true);               // 開起跑步開關
+    }
+}
